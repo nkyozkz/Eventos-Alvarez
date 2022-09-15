cee05e3b87f0c148bb4e63e00fa1db80aefce36a
nkyozkz <nicolas.unizkz@gmail.com>
2022-09-15 19:07:24 -0300

    subida de archivos

---
+++ productos.js
@@ -0,0 +1,11 @@
+const productos = [
+    {id:1, tipo:"Procesador", nombre:"Ryzen 3 3200G", precio: 12500, foto: "./media/1.png" },
+    {id:2, tipo:"Procesador", nombre:"Ryzen 5 3600", precio: 15700, foto: "./media/2.png" },
+    {id:3, tipo:"Procesador", nombre:"Ryzen 7 5800", precio: 20000, foto: "./media/3.png" },
+    {id:4, tipo:"Placa Madre", nombre:"ASUS ROG STRIX B550-F", precio: 23900, foto: "./media/4.png" },
+    {id:5, tipo:"Placa Madre", nombre:"ASUS TUF GAMING X570-PLUS", precio: 22000, foto: "./media/5.png" },
+    {id:6, tipo:"Placa Madre", nombre:"MSI A320M-A PRO MAX", precio: 4800, foto: "./media/6.png" },
+    {id:7, tipo: "Tarjeta de Video", nombre: "ASUS TUF-RTX3080TI", precio: 88500, foto: "./media/7.png"},
+    {id:8, tipo: "Tarjeta de Video", nombre: "EVGA GeForce GTX 1660 SUPER SC", precio: 30000, foto: "./media/8.png"},
+    {id:9, tipo: "Tarjeta de Video", nombre: "Galax GeForce GTX 1650 EX", precio: 199000, foto: "./media/9.png"},
+];
